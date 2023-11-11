195d1b8ede17e6132f6bf3694cb07295a27c60b6
Faraji Ombonya <farajiombonya@gmail.com>
2023-11-11 12:14:32 +0300

    feat:  app component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Authentication from './components/Authentication';
+import CashRegister from './components/CashRegister';
+import { useState } from 'react';
+
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const authToken = sessionStorage.getItem('authToken')
+
+  const handleAuthentication = () => {
+    setIsAuthenticated(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    isAuthenticated || authToken ?
+      <CashRegister authToken={authToken} /> :
+      <Authentication handleAuthentication={handleAuthentication} />
   );
 }
 
