ebdb345a904aad56f2b0e381f6a0341629bac075
Faraji Ombonya <farajiombonya@gmail.com>
2023-11-23 16:02:39 +0300

    refactor: authentication

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -3,46 +3,11 @@ import axios from "axios"
 import AuthActionButton from "./AuthActionButton"
 import "./Authentication.css"
 
-const apiUrl = process.env.REACT_APP_API_URL;
-
-// login helper function
-const login = async (credentials, handleAuthentication) => {
-    try {
-        console.log("sending login request")
-        const response = await axios.post(`${apiUrl}/api-token-auth/`, credentials)
-        sessionStorage.setItem("authToken", response.data.token);
-        sessionStorage.setItem("username", credentials.username);
-        sessionStorage.setItem("isAuthenticated", true);
-        handleAuthentication();
-    } catch (error) {
-        if (error.response && error.response.status === 400) {
-            alert("Incorrect username or password")
-        } else {
-            alert("An error occurred")
-            console.log(error)
-        }
-    }
-}
-
-// register helper function
-const register = async (credentials, isLogin, setIsLogin) => {
-    try {
-        const response = await axios.post(`${apiUrl}/users/`, credentials)
-        console.log(response)
-        setIsLogin(!isLogin)
-    } catch (error) {
-        if (error.response && error.response.status === 400) {
-            alert(error.response.data.detail)
-        } else {
-            alert("An error occurred")
-            console.log(error)
-        }
-    }
-}
 
 export default function Authentication({ handleAuthentication }) {
     const [credentials, setCredentials] = useState({ username: "", password: "" })
     const [isLogin, setIsLogin] = useState(true)
+    const apiUrl = process.env.REACT_APP_API_URL;
 
     const handleChange = (event) => {
         setCredentials({ ...credentials, [event.target.name]: event.target.value })
@@ -53,12 +18,45 @@ export default function Authentication({ handleAuthentication }) {
         setCredentials({ username: "", password: "" })
     }
 
+    // register helper function
+    const register = async (credentials, isLogin, setIsLogin) => {
+        try {
+            const response = await axios.post(`${apiUrl}/users/`, credentials)
+            console.log(response)
+            setIsLogin(!isLogin)
+        } catch (error) {
+            if (error.response && error.response.status === 400) {
+                alert(error.response.data.detail)
+            } else {
+                alert("An error occurred")
+                console.log(error)
+            }
+        }
+    }
+
+    // login helper function
+    const login = async () => {
+        try {
+            console.log("sending login request")
+            const response = await axios.post(`${apiUrl}/api-token-auth/`, credentials)
+            sessionStorage.setItem("authToken", response.data.token);
+            sessionStorage.setItem("username", credentials.username);
+            sessionStorage.setItem("isAuthenticated", true);
+            handleAuthentication();
+        } catch (error) {
+            if (error.response && error.response.status === 400) {
+                alert("Incorrect username or password")
+            } else {
+                alert("An error occurred")
+                console.log(error)
+            }
+        }
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault()
 
-        isLogin
-            ? login(credentials, handleAuthentication)
-            : register(credentials, isLogin, setIsLogin)
+        isLogin ? login() : register()
     }
 
     return (
